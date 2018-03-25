a6bf68603c2bfe775ccf00c78196f19fe61844b6
ok <przywarty.it@gmail.com>
2018-03-25 13:52:03 +0200

    Spawn knights as a function. Lock monitor functions with mutex. Fix dispalying knigths ids.

--- project/rastrum/src/main.rs
+++ project/rastrum/src/main.rs
@@ -1,34 +1,57 @@
-use std::sync::{Arc};
+use std::sync::{Arc, Condvar, Mutex};
 use std::{thread, time};
+
 const NTHREADS: usize = 4; 
 
 struct Rastrum {
-
+    monitor_lock: Mutex<bool>,
+    knigths_cvs: Vec<Condvar>,
 }
 
 impl Rastrum {
-    fn start_talking(&self) {
-        println!("Started!");
+    pub fn new() -> Rastrum{
+        let mut knigths_cvs = vec![];
+        for _ in 0..NTHREADS {
+            knigths_cvs.push(Condvar::new());
+        }
+        Rastrum {
+            monitor_lock: Mutex::new(false),
+            knigths_cvs: knigths_cvs,
+        }
     }
 
-    fn stop_talking(&self) {
-        println!("Stopped!");
+    fn start_talking(&self, knight_id : thread::ThreadId) {
+        let mutex = &self.monitor_lock;
+        *mutex.lock().unwrap();
+        println!("Started! id: {:?}", knight_id);
+        drop(mutex);
+    }
+
+    fn stop_talking(&self, knight_id : thread::ThreadId) {
+        let mutex = &self.monitor_lock;
+        *mutex.lock().unwrap();
+        println!("Stopped! id: {:?}", knight_id);
+        drop(mutex);
     }
 }
 
-fn main() {
-    let rastrum = Arc::new(Rastrum {});
+fn knight_main(r: Arc<Rastrum>) {
     let one_second = time::Duration::from_secs(1);
-    
+    let knight_id : thread::ThreadId = thread::current().id();
+    r.start_talking(knight_id);
+    thread::sleep(one_second);
+    r.stop_talking(knight_id);
+}
+
+fn main() {
+    let rastrum = Arc::new(Rastrum::new());
+
     let mut knights = vec![];
 
     for _ in 0..NTHREADS {
         let r = rastrum.clone();
         knights.push(thread::spawn(move || {
-            println!("Knight id: {:?}", thread::current().id());
-            r.start_talking();
-            thread::sleep(one_second);
-            r.stop_talking();
+            knight_main(r);
         }));
     }
 
