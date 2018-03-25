48dc26358c572b9cae113e3a41b408181e8570cf
ok <przywarty.it@gmail.com>
2018-03-25 22:14:58 +0200

    Fight with sharing mutable object between threads. Generally Rust disallows this, unless you will create a lock over this variable and thus it will ensure that calling mutable methods on such object will be atomic. It is impossible to call mutable methods without such operations, so since mutex is called over Monitor, I have deleted it from inside of the monitor.

--- project/rastrum/src/main.rs
+++ project/rastrum/src/main.rs
@@ -1,57 +1,74 @@
 use std::sync::{Arc, Condvar, Mutex};
 use std::{thread, time};
 
-const NTHREADS: usize = 4; 
+const NTHREADS: usize = 4;
+
+#[derive(Debug)]
+enum KnightState {
+    Idle,
+    Talking,
+    Willing,
+}
 
 struct Rastrum {
-    monitor_lock: Mutex<bool>,
-    knigths_cvs: Vec<Condvar>,
+    knights_cvs: Vec<Condvar>,
+    knight_states: Vec<KnightState>,
 }
 
 impl Rastrum {
     pub fn new() -> Rastrum{
-        let mut knigths_cvs = vec![];
-        for _ in 0..NTHREADS {
-            knigths_cvs.push(Condvar::new());
+        let mut knights_cvs = vec![];
+        let mut knight_states = vec![];
+        for _ in 1..NTHREADS+1 {
+            knights_cvs.push(Condvar::new());
+            knight_states.push(KnightState::Idle);
         }
         Rastrum {
-            monitor_lock: Mutex::new(false),
-            knigths_cvs: knigths_cvs,
+            knights_cvs: knights_cvs,
+            knight_states: knight_states,
         }
     }
 
-    fn start_talking(&self, knight_id : thread::ThreadId) {
-        let mutex = &self.monitor_lock;
-        *mutex.lock().unwrap();
-        println!("Started! id: {:?}", knight_id);
-        drop(mutex);
+    fn start_talking(&mut self, i : usize) {
+        println!("Started! id: {:?}", i);
+        self.knight_states[i%NTHREADS] = KnightState::Willing;
+        println!("Knight states {:?}", self.knight_states);
+        let (left, right) = (
+            &self.knight_states[(i-1)%NTHREADS],
+            &self.knight_states[(i+1)%NTHREADS],
+        );
+        println!("{:?} :: {:?}", left, right);
+        // if (left != KnightState::Talking
+        //     && right != KnightState::Talking)
     }
 
-    fn stop_talking(&self, knight_id : thread::ThreadId) {
-        let mutex = &self.monitor_lock;
-        *mutex.lock().unwrap();
-        println!("Stopped! id: {:?}", knight_id);
-        drop(mutex);
+    fn stop_talking(&self, i : usize) {
+        println!("Stopped! id: {:?}", i);
     }
 }
 
-fn knight_main(r: Arc<Rastrum>) {
+fn knight_main(i: usize, r: Arc<Mutex<Rastrum>>) {
     let one_second = time::Duration::from_secs(1);
-    let knight_id : thread::ThreadId = thread::current().id();
-    r.start_talking(knight_id);
+
+    let mut lock_start = r.lock().unwrap();
+    lock_start.start_talking(i);
+    drop(lock_start);
+
     thread::sleep(one_second);
-    r.stop_talking(knight_id);
+
+    let lock_stop = r.lock().unwrap();
+    lock_stop.stop_talking(i);
+    drop(lock_stop);
 }
 
 fn main() {
-    let rastrum = Arc::new(Rastrum::new());
-
+    let rastrum = Arc::new(Mutex::new(Rastrum::new()));
     let mut knights = vec![];
 
-    for _ in 0..NTHREADS {
+    for i in 1..NTHREADS+1 {
         let r = rastrum.clone();
         knights.push(thread::spawn(move || {
-            knight_main(r);
+            knight_main(i, r);
         }));
     }
 
